6931623f6f16f10cf5ddb58c6aa28dcf3374690e
Apostolos Smyrnakis <apostolos.smyrnakis@cern.ch>
2019-10-23 00:09:20 +0200

    add OTA firmware update

--- rjdMonitor.ino
+++ rjdMonitor.ino
@@ -5,7 +5,8 @@
 #include <WiFiManager.h>
 #include <NTPClient.h>
 #include "DHT.h"
-#include <PubSubClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include "secrets.h"
 
 #define DHTTYPE DHT11
@@ -70,6 +71,7 @@ void setup() {
   randomSeed(analogRead(0));
 
   Serial.begin(115200);
+  delay(100);
 
   WiFiManager wifiManager;
   //wifiManager.resetSettings();
@@ -91,7 +93,48 @@ void setup() {
 
   dht.begin();
 
-  delay(500);
+  // while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  //   Serial.println("Connection Failed! Rebooting...");
+  //   delay(5000);
+  //   ESP.restart();
+  // }
+
+  // handle OTA updates
+  handleOTA();
+
+  delay(400);
+}
+
+
+//
+void handleOTA() {
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
 }
 
 
@@ -244,6 +287,9 @@ void serialPrintAll() {
 
 
 void loop(){
+
+  // Handle OTA firmware updates
+  ArduinoOTA.handle();
 
   unsigned long currentMillis = millis();
 
